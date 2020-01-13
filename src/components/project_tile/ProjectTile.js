88aa65f56e33be409da80fa7505dd77e74cba23c
haydnwiese <45501692+haydnwiese@users.noreply.github.com>
2020-01-13 15:02:04 -0500

    Decreased project tile size and added new logos to resources

--- src/components/project_tile/ProjectTile.js
+++ src/components/project_tile/ProjectTile.js
@@ -5,6 +5,8 @@ import './ProjectTile.css'
 import reactLogo from '../../resources/projects/react.png';
 import appScreenshot1 from '../../resources/projects/coopScreenshot1.png';
 import appScreenshot2 from '../../resources/projects/coopScreenshot2.png';
+import androidLogo from '../../resources/projects/androidLogo.png';
+import nodeLogo from '../../resources/projects/nodeLogo.png';
 
 export default class ProjectTile extends React.Component {
     constructor(props) {
@@ -23,17 +25,28 @@ export default class ProjectTile extends React.Component {
     render() {
         let className;
         this.state.active ? className = 'projectTile projectTileActive' : className = 'projectTile';
+        let toolIcon;
+        switch (this.props.projectTool) {
+            case 'React Native':
+                toolIcon = reactLogo;
+                break;
+            case 'Android (Java)':
+                toolIcon = androidLogo;
+                break;
+            case 'Node.js':
+                toolIcon = nodeLogo;
+                break;   
+            default:
+                toolIcon = reactLogo;        
+        }
 
         if (!this.state.active) {
             return(
                 <div className={className} onClick={() => this.state.active ? this.setState({active: false}) : this.setState({active: true})}>
                     <div className='projectTitle'>{this.props.projectTitle}</div>
                     <div className='projectToolContainer'>
-                        <img className='projectToolLogo' src={reactLogo} />
+                        <img className='projectToolLogo' src={toolIcon} />
                         <div className='projectTool'>{this.props.projectTool}</div>
-                    </div>
-                    <div className={`previewContainer ${this.props.layout}`}>
-                        {this.props.children}
                     </div>
                     <div className='promptContainer'>
                         <div className='expandPrompt'>{this.state.active ? 'Click to Collapse' : 'Click to Expand'}</div>
@@ -45,7 +58,7 @@ export default class ProjectTile extends React.Component {
                 <div className={className} onClick={() => this.state.active ? this.resetTile() : this.setState({active: true})}>
                     <div className='projectTitle'>{this.props.projectTitle}</div>
                     <div className='projectToolContainer'>
-                        <img className='projectToolLogo' src={reactLogo} />
+                        <img className='projectToolLogo' src={toolIcon} />
                         <div className='projectTool'>{this.props.projectTool}</div>
                     </div>
                     <div className={`previewContainer ${this.props.layout}`}>
